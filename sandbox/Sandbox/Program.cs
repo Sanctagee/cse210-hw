08c0283578f843731ccc02118829ee6bec22997e
SanctaGee <tonygabito@gmail.com>
2025-07-08 13:48:26 +0100

    finished prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        // Ask for first name
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        // Ask for last name
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        // Display formatted output
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
