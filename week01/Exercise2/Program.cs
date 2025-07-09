a65888cf0c77190564517a68a36df4b4d5e8166d
SanctaGee <tonygabito@gmail.com>
2025-07-09 09:06:37 +0100

    Core requirements for the Exercise2 week01 assignment met

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask for grade percentage
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string letter = ""; // Variable to hold the letter grade
+
+        // Determine letter grade
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        // Print letter grade
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        // Check if user passed
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard for next time!");
+        }
+
     }
 }
