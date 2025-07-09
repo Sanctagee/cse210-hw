be0c4d89ad9db23d1d39bc466e3745016d0ee845
SanctaGee <tonygabito@gmail.com>
2025-07-09 09:53:11 +0100

    Core Requirements for Exercise 3 met

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Generate random magic number (1–100)
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess;
+        int guessCount = 0; // Track guesses for stretch challenge
+
+        Console.WriteLine("Guess the magic number (1-100)!");
+
+        do
+        {
+            // Get user guess
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            // Provide feedback
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it in {guessCount} tries!");
+            }
+        } while (guess != magicNumber);
     }
 }
