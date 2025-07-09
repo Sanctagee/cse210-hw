79103fcb1886c98e83659779e5a65c831e55d0c6
SanctaGee <tonygabito@gmail.com>
2025-07-09 10:25:40 +0100

    stretch challenge for this exercise met

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,35 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Generate random magic number (1–100)
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
+        //  Declaring variables that will enhance the loop
+        string playAgain = "yes";
 
-        int guess;
-        int guessCount = 0; // Track guesses for stretch challenge
+        while (playAgain == "yes")
 
-        Console.WriteLine("Guess the magic number (1-100)!");
-
-        do
         {
-            // Get user guess
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
-            guessCount++;
+            // Generate random magic number (1–100)
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess;
+            int guessCount = 0; // Track guesses for stretch challenge
 
-            // Provide feedback
-            if (guess < magicNumber)
+            Console.WriteLine("\nGuess the magic number (1-100)!");
+
+            do
             {
-                Console.WriteLine("Higher");
-            }
-            else if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine($"You guessed it in {guessCount} tries!");
-            }
-        } while (guess != magicNumber);
+                // Get user guess
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} tries!");
+                }
+            } while (guess != magicNumber);
+
+            // Ask to play again
+            Console.Write("\nPlay again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        }
+
+        Console.WriteLine("Thanks for playing!");
+            
+
     }
 }
